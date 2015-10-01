d391bccde5f9dc1230c451637dda468bb07ffff6
tony1511 <tony88.silver@gmail.com>
2015-10-01 07:42:43 +0200

    login implemented

---
+++ RestoranMIS_UI/Frm_LoginForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace RestoranMIS_UI
+{
+    partial class Frm_LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.logBtn = new System.Windows.Forms.Button();
+            this.errLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // userBox
+            // 
+            this.userBox.Location = new System.Drawing.Point(16, 30);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(210, 20);
+            this.userBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(19, 74);
+            this.passBox.Name = "passBox";
+            this.passBox.PasswordChar = '*';
+            this.passBox.Size = new System.Drawing.Size(207, 20);
+            this.passBox.TabIndex = 3;
+            // 
+            // logBtn
+            // 
+            this.logBtn.Location = new System.Drawing.Point(269, 30);
+            this.logBtn.Name = "logBtn";
+            this.logBtn.Size = new System.Drawing.Size(156, 40);
+            this.logBtn.TabIndex = 4;
+            this.logBtn.Text = "Login";
+            this.logBtn.UseVisualStyleBackColor = true;
+            this.logBtn.Click += new System.EventHandler(this.logBtn_Click);
+            // 
+            // errLabel
+            // 
+            this.errLabel.AutoSize = true;
+            this.errLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.errLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.errLabel.ForeColor = System.Drawing.Color.Red;
+            this.errLabel.Location = new System.Drawing.Point(300, 81);
+            this.errLabel.Name = "errLabel";
+            this.errLabel.Size = new System.Drawing.Size(103, 13);
+            this.errLabel.TabIndex = 5;
+            this.errLabel.Text = "Login Unsuccessful!";
+            this.errLabel.Visible = false;
+            // 
+            // Frm_LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 128);
+            this.Controls.Add(this.errLabel);
+            this.Controls.Add(this.logBtn);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Frm_LoginForm";
+            this.Text = "Frm_LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Button logBtn;
+        private System.Windows.Forms.Label errLabel;
+    }
+}
---
+++ RestoranMIS_UI/Frm_LoginForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestoranMIS_DAL;
+
+namespace RestoranMIS_UI
+{
+    public partial class Frm_LoginForm : Form
+    {
+        public Frm_LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void logBtn_Click(object sender, EventArgs e)
+        {
+            if (DAEmployees.GetUserByLogin(userBox.Text, passBox.Text))
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                errLabel.Visible = true;
+            }
+            /*if (DAEmployees.GetEmployeeByLogin(txt_Username.Text, txt_Pass.Text))
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                lbl_warning.Visible = true;
+                lbl_warning.Text = "Pogresan username ili password!";
+            }*/
+        }
+    }
+}
